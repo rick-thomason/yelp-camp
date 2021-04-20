0d079b24b258df0bc498087edcc8711f0e1ccd14
Rick <67919881+BuRNoceros@users.noreply.github.com>
2021-04-20 13:36:23 -0400

    connect mongo to server with express and make campground schema

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+})
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connecton error:'))
+db.once('open', () => {
+  console.log('DATABASE CONNECTED')
+})
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(5000, () => {
+  console.log('SERVER IS LISTENING ON PORT 5000...')
+})
