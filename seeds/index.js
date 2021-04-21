d5a9dfa79ee18637f073f1ba7c0d5058b6322092
Rick <67919881+BuRNoceros@users.noreply.github.com>
2021-04-21 15:07:14 -0400

    add images description and price to show page

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,14 @@ const seedDB = async () => {
   await Campground.deleteMany({})
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000)
+    const price = Math.floor(Math.random() * 20) + 10
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis autem ducimus iste dolores. Asperiores placeat alias eligendi, distinctio fugit nulla facilis cum impedit iure atque possimus, quam eos inventore sunt aut porro nemo magni, blanditiis dignissimos molestias ab repellat! Natus quis omnis exercitationem labore eius qui debitis praesentium animi optio.',
+      price,
     })
     await camp.save()
   }
