0837d1f13dc67b4f8f0b562dd5825ce161d9db8f
Rick <67919881+BuRNoceros@users.noreply.github.com>
2021-05-16 10:55:28 -0400

    link yelpcamp to cloudinary to store files

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,7 +4,8 @@ const campgrounds = require('../controllers/campgrounds')
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware')
 const catchAsync = require('../utils/catchAsync')
 const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const { storage } = require('../cloudinary')
+const upload = multer({ storage })
 
 router
   .route('/')
