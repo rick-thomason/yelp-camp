558907f14cec8b047a3c090899e345b1854bd1e5
Rick <67919881+BuRNoceros@users.noreply.github.com>
2021-05-16 10:25:14 -0400

    add feature for adding files to new campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,15 +3,21 @@ const router = express.Router()
 const campgrounds = require('../controllers/campgrounds')
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware')
 const catchAsync = require('../utils/catchAsync')
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 router
   .route('/')
   .get(catchAsync(campgrounds.index))
-  .post(
-    validateCampground,
-    isLoggedIn,
-    catchAsync(campgrounds.createNewCampground)
-  )
+  // .post(
+  //   validateCampground,
+  //   isLoggedIn,
+  //   catchAsync(campgrounds.createNewCampground)
+  // )
+  .post(upload.array('image'), (req, res) => {
+    console.log(req.body, req.files)
+    res.send('it worked')
+  })
 
 router.get('/new', isLoggedIn, catchAsync(campgrounds.renderNewForm))
 
